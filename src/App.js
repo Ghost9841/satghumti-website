7324413a6e90a503c468930cf8ebec3098bc9a53
Ghost9841 <asubedi778@gmail.com>
2024-09-14 18:31:54 +0545

    button transition

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Home from './Pages/Home';
 import Navbar from './Components/Navbar';
 import About from './Pages/About';
+import Services from './Pages/Services';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
       <Routes>
         <Route path = "/" element = {<Home/>}/>
         <Route path = "/about" element = {<About/>}/>
+        <Route path = "/services" element = {<Services/>}/>
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { assets } from '../assets/assets';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="footer-logo">
+            <img src= {assets.logo}alt=""  />
+        </div>
+    </div>
+  )
+}
+
+export default Footer;
